6f9afcbfe4fc7d824bb4ffa068a652ce2075bae7
manikandan-v <vmmass08gmail.com>
2022-06-01 15:55:18 +0530

    add-intex.

---
+++ script.js
@@ -0,0 +1,10 @@
+
+//2, 5 divisible Number
+
+let b=40;
+if(b%5==0){
+    console.log("This Number Is Divisible")
+}
+else{
+    console.log(" This Number Is Not Divisible");                                                                                
+ }
